1918060025c636899698310c3d97822c43a2348d
yves3a <yves.irakoz3@gmail.com>
2024-06-23 09:48:47 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - the program that will display with puts command
+ * return: 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
