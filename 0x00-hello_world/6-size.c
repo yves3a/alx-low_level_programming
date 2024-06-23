8e4aa133ffe4b6f5326f6b12d554df11a10223ff
yves3a <yves.irakoz3@gmail.com>
2024-06-23 15:52:35 -0700

    sixth

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that will print sizes of functions
+ * Return: returns 0 (sucess)
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	prinf("Size of an int: 4 byte(s)\n");
+	prinf("Size of a long int: 4 byte(s)\n");
+	prinf("Size of a long long int: 8 byte(s)\n");
+	prinf("Size of a float: 4 byte(s)\n");
+	prinf("Size of an int: 4 byte(s)\n");
+	return (0);
+
+}
