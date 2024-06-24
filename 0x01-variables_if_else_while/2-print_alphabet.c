61ee87a88fec32847035ef9d99d8dd6804062a22
yves3a <yves.irakoz3@gmail.com>
2024-06-24 13:56:26 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that will print a alphabetic
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'a';
+
+	while
+		(d <= 'z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
