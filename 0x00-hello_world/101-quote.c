a818046802675b651ebaf54a97143b37e94a3bdb
yves3a <yves.irakoz3@gmail.com>
2024-06-24 08:34:51 -0700

    Intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A program that prints a line to a standard error
- * return 1 (success)
+ * return: 1(success)
  */
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
