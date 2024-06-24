92f7260ad8c146f2ae4ab44099283c2d0e757d73
yves3a <yves.irakoz3@gmail.com>
2024-06-24 06:26:45 -0700

    Sizes of functions

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,18 @@
  */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s)\n");
-	prinf("Size of an int: 4 byte(s)\n");
-	prinf("Size of a long int: 4 byte(s)\n");
-	prinf("Size of a long long int: 8 byte(s)\n");
-	prinf("Size of a float: 4 byte(s)\n");
-	prinf("Size of an int: 4 byte(s)\n");
+	char a;
+	int b;
+	long long int c;
+	long int d;
+	float e;
+
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n"(unsigned long)size of(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 
 }
