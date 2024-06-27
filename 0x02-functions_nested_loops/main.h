0254e8b5f5df7cff24e22ecbdf5bd4b20c206806
yves3a <yves.irakoz3@gmail.com>
2024-06-27 07:53:55 -0700

    first commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_*10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+void print_time_table(int n); 
+#endif
