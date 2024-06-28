cb360452676aefe0f53285007853681a9d371d97
yves3a <yves.irakoz3@gmail.com>
2024-06-28 02:41:43 -0700

    functions in c

--- 0x02-functions_nested_loops/0-isupper.c
+++ 0x02-functions_nested_loops/0-isupper.c
@@ -1,17 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	char c;	
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-       	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * puchar -writes the character c to stdout
+ * @c:The character to print
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
