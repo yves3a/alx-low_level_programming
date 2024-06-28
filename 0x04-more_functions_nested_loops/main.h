14cc888786aa21fb4b54cd53b8a1af37a0b0e2f2
yves3a <yves.irakoz3@gmail.com>
2024-06-28 06:39:40 -0700

    more functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifndef main_h
-#define main_h
-int main (void);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,4 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
 #endif
