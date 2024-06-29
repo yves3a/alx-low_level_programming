cd1cd8925e2fc576a4644758d126f4fda65c3ddd
yves3a <yves.irakoz3@gmail.com>
2024-06-29 12:18:45 -0700

    draws a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * @n: number of times the character should be printed
+ * Return: a straight line
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('-');
+	}
+	_putchar('\n');
+}
