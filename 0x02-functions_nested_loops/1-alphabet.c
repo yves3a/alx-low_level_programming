7d0b11c7241ba6416d00011bc37ac4ec8b951e82
yves3a <yves.irakoz3@gmail.com>
2024-07-01 09:19:48 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a functions that prints a alphabets
+ * Return: 0 on success execution
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
