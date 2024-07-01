bb334768680a10ce449c0aeb9b30ebe8ccdba643
yves3a <yves.irakoz3@gmail.com>
2024-07-01 11:21:50 -0700

    absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - a function that computes a absolute value of interger
+ *@n: number
+ * Return: returns interger on successful execution
+ */
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+		return (n);
+
+}
