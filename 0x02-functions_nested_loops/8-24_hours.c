878ba609e1cfec273be247af7b876db3bef36511
yves3a <yves.irakoz3@gmail.com>
2024-07-01 12:21:26 -0700

    hours and minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#inlude "main.h"
+/**
+ * jack_bauer - prints hours and minutes
+ * Return:
+ */
+void jack_bauer(void)
+{
+	int hr;
+	int min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+		}
+	}
+}
