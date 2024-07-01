de5184c5c8e7e55bb22f8e56d16c32ac5075ed34
yves3a <yves.irakoz3@gmail.com>
2024-07-01 08:24:16 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - a function that prints _putchar
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_';
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
