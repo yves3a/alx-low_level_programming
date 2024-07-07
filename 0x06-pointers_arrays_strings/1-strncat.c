948014957a50b8774afe0395987c78bdfd9d7a69
yves3a <yves.irakoz3@gmail.com>
2024-07-07 14:52:13 -0700

    pause

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strncat - a function that concatenates two strings
+ * @dest: to be concatenated with src
+ * @src: to be transferred to dest
+ * @n: at most bytes from src to go.
+ * Return: dest when successfully
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	for (i; dest[i] != '\0'; i++)
+	{
+		dest[i] = dest[i];
+	}
+	for (j = 0; src[j] != n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	return (dest);
+}
