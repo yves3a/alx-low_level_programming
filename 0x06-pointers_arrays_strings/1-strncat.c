b64ff61c1edc30b7c8d23603c043edd27868e5e9
yves3a <yves.irakoz3@gmail.com>
2024-07-08 04:08:24 -0700

    updates

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,11 +14,14 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (; dest[i] != '\0'; i++)
 	{
-		dest[i] = dest[i];
+		;
 	}
-	for (j = 0; src[j] != n; j++)
+	dest[i] = '\0';
+
+	for (j = 0; j<n && src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
 	}
+	dest[i+j] = '\0';
 	return (dest);
 }
