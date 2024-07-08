1792b6c160477b29c36cf8230f92ef9fefdccbf3
yves3a <yves.irakoz3@gmail.com>
2024-07-08 05:45:47 -0700

    copying strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strncpy - a function that copies a strings
+ * @dest: this is where copied source code will be pasted
+ * @src: strings to be copied
+ * @n: at most number of characters of src to be copied.
+ * Return: dest wehen successfully executed
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
