9b052b5221ae259be643749248bae4d5b1735098
yves3a <yves.irakoz3@gmail.com>
2024-07-22 08:52:31 -0700

    static libraries

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, sign, n, len, f, digit;
+
+	i = 0;
+	sign = 1;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			sign = -sign;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			digit = sign * digit;
+			n = n * 10 + digit;
+			f = 1;
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
