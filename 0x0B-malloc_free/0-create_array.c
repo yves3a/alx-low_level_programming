01537af6b10c281b742558e1aa9a7e59a3d32ae2
yves3a <yves.irakoz3@gmail.com>
2024-07-25 03:37:14 -0700

    malloc free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,10 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int index;
 
-	ptr = malloc(size * sizeof(char));
 
 	if (size == 0)
 		return (NULL);
-	 ptr = malloc(size * sizeof(char));
+	ptr = malloc(size * sizeof(char));
 
 	if (ptr == NULL)
 		return (NULL);
