8a1a3215b76644a2e60324ff6e9475cc9c1f72eb
yves3a <yves.irakoz3@gmail.com>
2024-07-25 05:51:44 -0700

    copying the string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - a function that returns a pointer to a newly
+ *          allocated memory with the string
+ * @str: string to be taken into the memory
+ * Return: - NULL if the string is null and when insuffiecient memory.
+ *         - a pointer to the string
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int index = 0;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[index] != '\0')
+		len++;
+
+	ptr = malloc((len * sizeof(char)) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		ptr[index] = str[index];
+	}
+	ptr[index] = '\0';
+
+
+	return (ptr);
+}
