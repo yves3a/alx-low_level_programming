89b39f103cca9535fb7c22ea7eb17fd2c1f092be
yves3a <yves.irakoz3@gmail.com>
2024-07-28 20:19:22 +0200

    wowww

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc((size + 1) * sizeof(int));
+	ptr = (int *)malloc((size + 1) * sizeof(int));
 
 	if (ptr == NULL)
 		return (NULL);
