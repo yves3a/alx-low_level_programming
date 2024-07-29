88b7eafd51c32533b839b12abe96cbfaa2a39a78
yves3a <yves.irakoz3@gmail.com>
2024-07-29 14:50:25 +0200

    not at all

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * mul - multiplies 2 numbers
  * @n1: the 1st number
  * @n2: the second number
- * Return: the products of 2 numbers
+ * Return: the product of 2 numbers
  */
 
 int mul(int n1, int n2)
 {
 	return (n1 * n2);
+}
+
+/**
+ * is_digit_str - checks if a string is composed of digits
+ * @str: the string to check
+ * Return: 1 if true, 0 if false
+ */
+
+int is_digit_str(char *str)
+{
+	while (*str)
+	{
+		if (!isdigit(*str))
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
 }
 
 /**
@@ -30,10 +50,10 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (!atoi(argv[1]) || !atoi(argv[2]))
+	if (!is_digit_str(argv[1]) || !is_digit_str(argv[2]))
 	{
-		printf("Error\n");
-		exit(98);
+		 printf("Error\n");
+		 exit(98);
 	}
 
 	n1 = atoi(argv[1]);
