281b1cead8a9033a4e196430762a51d204dcd863
yves3a <yves.irakoz3@gmail.com>
2024-08-08 15:52:41 +0200

    Freeing the allocated memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - it frees dogs
+ * @d: the dog to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	/*freeing the allocated memory address */
+
+	if (d == NULL)
+		free(d);
+	if (d->name == NULL)
+		free(d->name);
+
+	if (d->owner == NULL)
+		free(d->owner);
+	else
+		return;
+}
