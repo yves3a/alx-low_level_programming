81d397249316a36f4da8bbfe3842fa37ee929d65
yves3a <yves.irakoz3@gmail.com>
2024-08-22 14:05:50 +0200

    reading from file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,19 +10,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
+	char *buff;
+	int fd;
 	ssize_t w;
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	t = read(fd, buff, letters);
+	w = write(STDOUT_FILENO, buff, t);
 
-	free(buf);
+	free(buff);
 	close(fd);
 	return (w);
 }
