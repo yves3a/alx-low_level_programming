f9fd65429f46e3f31e280f34beee7af71f114595
yves3a <yves.irakoz3@gmail.com>
2024-10-04 15:19:17 +0200

    Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_dlistint - a function that returns a number ofthe project
+ * @h: a pointer to the first node
+ *
+ * Return: number of nodes
+ */ 
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int index;
+	const dlistint_t *ptr;
+	ptr = h;
+
+	for (index = 0; ptr != NULL; index++)
+	{
+		printf("%d\n", h->n);
+		ptr = ptr->next;
+	}
+
+	return (index);
+}
