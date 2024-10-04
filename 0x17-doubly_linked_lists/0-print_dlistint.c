d3063c81fc723d65f73fa345b6174c1e11db4105
yves3a <yves.irakoz3@gmail.com>
2024-10-04 15:28:35 +0200

    Print a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,13 @@
  * @h: a pointer to the first node
  *
  * Return: number of nodes
- */ 
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	int index;
 	const dlistint_t *ptr;
+
 	ptr = h;
 
 	for (index = 0; ptr != NULL; index++)
